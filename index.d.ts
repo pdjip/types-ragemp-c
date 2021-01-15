3f4ae26e1d8cfc765452e75a16531e55a27e781d
blubbel <paul@jippa.de>
2021-01-15 20:49:34 +0100

    added mp.game.graphics.setLightsState

--- index.d.ts
+++ index.d.ts
@@ -175,7 +175,7 @@ interface EntityMp {
 		boneIndex: number, isRel: number, p9: boolean, highForce: boolean, p11: boolean, p12: boolean): void;
 	applyForceToCenterOfMass(forceType: number, x: number, y: number, z: number, p4: boolean, isRel: boolean,
 		highForce: boolean, p7: boolean): void;
-	attachTo(entity: Handle, boneIndex: number, xPos: number, yPos: number, zPos: number, xRot: number, yRot: number, 
+	attachTo(entity: Handle, boneIndex: number, xPos: number, yPos: number, zPos: number, xRot: number, yRot: number,
 		zRot: number, p8: boolean, useSoftPinning: boolean, collision: boolean, isPed: boolean, vertexIndex: number,
 		fixedRot: boolean): void;
 	attachToPhysically(entity: Handle, boneIndex1: number, boneIndex2: number, xPos1: number, yPos1: number,
@@ -373,7 +373,7 @@ interface PedMp extends EntityMp {
 }
 
 interface PickupMp extends EntityMp {
-	
+
 }
 
 interface PlayerMp extends EntityMp {
@@ -1065,7 +1065,7 @@ interface VehicleMp extends EntityMp {
 	gear: number;
 	rpm: number;
 	steeringAngle: number;
-	
+
 	addUpsidedownCheck(): void;
 	areAllWindowsIntact(): boolean;
 	attachToCargobob(cargobob: Handle, p1: number, x: number, y: number, z: number): void;
@@ -2256,6 +2256,7 @@ interface GameGraphicsMp {
 	world3dToScreen2d(worldX: number, worldY: number, worldZ: number): {
 		x: number; y: number;
 	};
+	setLightsState(id: number, toggle: boolean);
 }
 
 interface GameInteriorMp {
